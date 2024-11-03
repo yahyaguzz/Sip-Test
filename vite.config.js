df1d2d9522e2e140ce90c75a1861b41de8a156d1
yahyaguzz <yahyag@actstel.com>
2024-11-03 03:37:34 +0300

    Add vitejs/plugin-basic-ssl package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    https: true, // HTTPS üzerinden çalışacak
+    host: '0.0.0.0', // Tüm IP adreslerinden erişime aç
+    port: 5173, // İsteğe bağlı olarak başka bir port numarası verebilirsin
+  },
+  plugins: [react(), basicSsl()],
 })
